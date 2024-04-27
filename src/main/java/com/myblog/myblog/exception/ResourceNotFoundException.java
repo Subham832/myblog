3a571aeb02c18d008687f51cfedf3eb5a9f88753
Subham832 <163394796+Subham832@users.noreply.github.com>
2024-04-27 16:26:48 +0530

    Delete src/main/java/com/myblog/myblog/exception directory

--- src/main/java/com/myblog/myblog/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.myblog.myblog.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
